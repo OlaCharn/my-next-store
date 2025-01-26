43f7c41f9f0087113f59fc3e1218c53112a00c90
OlaCharn <volha.charnukha@gmail.com>
2025-01-26 18:52:01 +0100

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <h1 className='text-3x1' >Homepage</h1>
+      <Button variant="outline" size="lg" className='capitalize m-8' >
+        Click me
+      </Button>
+    </div>
   )
 }
 
-export default Homepage
+export default Homepage;
