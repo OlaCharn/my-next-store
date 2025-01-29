d6932e8dd7a4e3d56077ae35712e35d2a44620d1
OlaCharn <volha.charnukha@gmail.com>
2025-01-29 18:44:52 +0100

    profile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import db from "@/utils/db";
 
-function AboutPage() {
+async function AboutPage() {
+  const profile = await db.testProfile.create({
+    data: {
+      name: "random nema",
+    },
+  });
+  const users = await db.testProfile.findMany();
+
   return (
-    <div>AboutPage</div>
-  )
+    <div>
+      {users.map((user) => {
+        return (
+        <h2 key={user.id} className="text 2xl font-bold" >
+          {user.name}
+        </h2>
+        );
+      })}
+    </div>
+    );
 }
 
 export default AboutPage;
